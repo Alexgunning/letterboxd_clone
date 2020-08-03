6214c748141d6a8b50eca6ba6e91604329cd75dc
Alexander Gunning <alexandergunning@Alexanders-MacBook-Pro.local>
2020-08-03 09:17:47 -0600

    Connect frontend to backend

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, ApolloProvider, InMemoryCache, useQuery } from '@apollo/client';
+import { gql } from '@apollo/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+const MOVIES = gql`
+      query GetMovies {
+        movies {
+          id
+          title,
+          year
+        }
+      }
+`;
+
+  function Movies() {
+    const { loading, error, data } = useQuery(MOVIES);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    console.log('alex');
+    console.log(data.movies);
+  
+    // return data.movies.map(({ id: Number, title: String, year: Number }) => (
+    return data.movies.map((obj:any) => (
+      <div key={obj.id}>
+        <p>
+          {obj.title}: {obj.year}
+        </p>
+      </div>
+    ));
+  }
+
+  function App() {
+    return (
+      <ApolloProvider client={client}>
+        <div>
+          <Movies />
+        </div>
+      </ApolloProvider>
+    );
+  }
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- server/api/app.ts
+++ server/api/app.ts
@@ -1,4 +1,6 @@
-import { use } from 'nexus'
+import { use, server } from 'nexus'
 import { prisma } from 'nexus-plugin-prisma'
+import cors from 'cors';
+server.express.use(cors())
 use(prisma({ features: { crud: true } }));
 
